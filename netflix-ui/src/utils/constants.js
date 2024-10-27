3172ca15083f27ba30d8a5df157f814004c1c86b
Marcelino Saquende <102360616+msaquende18@users.noreply.github.com>
2024-10-27 23:59:37 +0100

    Update constants.js

--- netflix-ui/src/utils/constants.js
+++ netflix-ui/src/utils/constants.js
@@ -1,4 +1,4 @@
 export const API_KEY = "3d39d6bfe362592e6aa293f01fbcf9b9";
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
-export const LOCAL_BASE_URL = "localhost:3000";
+export const LOCAL_BASE_URL = "https://localhost:3000";
 
